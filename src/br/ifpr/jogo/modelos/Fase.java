0dd4ced993632763c2a2363f9b3bf9d565d82ca4
Laboratorio_IFPR <estudante@ifpr.edu.br>
2023-05-23 22:47:39 -0300

    Hamster heroina adicionado ao fundo

---
+++ src/br/ifpr/jogo/modelos/Fase.java
@@ -0,0 +1,10 @@
+package br.ifpr.jogo.modelos;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundoImagem;
+
+    public Fase(){
+        ImageIcon carregando = new ImageIcon("");    
+    }
